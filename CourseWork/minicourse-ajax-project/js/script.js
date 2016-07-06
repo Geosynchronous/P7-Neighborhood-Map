a00e5d9f5d8678099f2d576928409bcc9be26432
George Fischer <geosynchronous@mac.com>
2016-07-05 20:55:25 -0600

    feature: Quiz - Streetview
    User types in address and changes background

--- CourseWork/minicourse-ajax-project/js/script.js
+++ CourseWork/minicourse-ajax-project/js/script.js
@@ -15,6 +15,20 @@ function loadData() {
 
     // YOUR CODE GOES HERE!
 
+    // Gets values from form inputs and creates a var address string
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ',' + cityStr;
+
+    // Replace original greeting "Where do you want to live at?"
+    $greeting.text('So you want to live at ' + address + '?');
+
+    // Concatenate address with base URL to create the streetviewURL with adress parameters
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '';
+
+    // Appends a background image of the streetmap address location to the body
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '" >');
+
     return false;
 };
 
