7f993ff498971f103e2b6271d8b0429df9031619
George Fischer <geosynchronous@mac.com>
2016-07-06 22:21:04 -0600

    feat: Adding NYT Articles
    Request Data from NYT, list to website

--- CourseWork/minicourse-ajax-project 2/js/script.js
+++ CourseWork/minicourse-ajax-project 2/js/script.js
@@ -24,8 +24,33 @@ function loadData() {
 
 
     // load nytimes
-    
+
     // YOUR CODE GOES HERE!
+    // URL for NYT article search with my registered API key
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=4e6e2405a9e14cfc9121ccdcad44a03f';
+
+    // Request Article Search from NYT, then parses articles data into list
+    $getJSON(nytimesUrl, function(data)){
+
+        // Write out Header for List of Articles from Searched City
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        // Load the NYT RESPONSE data for docs elements into articles array elements
+        articles = data.response.docs
+        // Make a list of every article and corresponding snippet
+        for (var i=0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class ="article">' +
+                '<a href>"'+article.web_url+'">'+article.headline.main+
+                    '</a>' +
+                    '<p>' + article.snippet + '</p>' +
+            '</li>');
+        };
+
+    };
+
+
+
 
     return false;
 };
