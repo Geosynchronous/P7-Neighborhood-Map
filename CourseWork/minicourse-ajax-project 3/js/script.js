54bd0b303bb3af52b6c3679c561df37558aab5d3
George Fischer <geosynchronous@mac.com>
2016-07-08 13:34:49 -0600

    fix: Added NYT API KEY

--- CourseWork/minicourse-ajax-project 3/js/script.js
+++ CourseWork/minicourse-ajax-project 3/js/script.js
@@ -28,7 +28,7 @@ function loadData() {
 
     // load nytimes
     // obviously, replace all the "X"s with your own API key
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=4e6e2405a9e14cfc9121ccdcad44a03f';
     $.getJSON(nytimesUrl, function(data){
 
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
@@ -67,7 +67,7 @@ function loadData() {
                 var url = 'http://en.wikipedia.org/wiki' + articleStr;
                 $wikiElem.append('<li><a href="' + url + '">' + articleStr +  '</a></li>');
             };
-        }
+        };
     });
 
 
