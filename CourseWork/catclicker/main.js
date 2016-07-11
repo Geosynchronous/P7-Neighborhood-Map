7785297a376e49ea8f5375cc1ed10a04aab600f5
George Fischer <geosynchronous@mac.com>
2016-07-11 09:05:22 -0600

    refactor: Cat Clicker
    Cleaned up code and design

--- CourseWork/catclicker/main.js
+++ CourseWork/catclicker/main.js
@@ -1,20 +1,29 @@
+// Initialize Click Counter value
+var count = 0;
+
+// Get DOM NODES for HTML Elements
 var elemCat = document.getElementById('tiger');
 var elemCounter = document.getElementById('counter');
 
-var count = 0;
-var para = document.createElement("p");                       // Create a <p> node
 
+// Create DOM HTML Element Nodes
+// Create a generic <p> node
+var para = document.createElement("p");
+// Create a text node
+var t = document.createTextNode(count);
 
+// Append Nodes
+// Append the text to <p>
+para.appendChild(t);
+// Append <p> to <div>
+elemCounter.appendChild(para);
+
+// Updates Click Count View
 elemCat.addEventListener('click', function(){
-  //the element has been clicked... do stuff here
+	// the element has been clicked...
 	count++;
-
-	var t = document.createTextNode(count);      				  // Create a text node
-	para.appendChild(t); 											// Append the text to <p>
-	elemCounter.appendChild(para); 									// Append <p> to <div>
-
+	// Overwrites previous Count
 	para.innerHTML = count;
-
 
 
 }, false);
