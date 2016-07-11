378114cd85539228d0c9f468de73d74b8caf51b8
George Fischer <geosynchronous@mac.com>
2016-07-11 10:04:08 -0600

    chore: Create Cat Clicker Dual starter files

---
+++ CourseWork/catclicker-dual/main.js
@@ -0,0 +1,29 @@
+// Initialize Click Counter default value
+var count = 0;
+
+// Get DOM NODES for HTML Elements
+var elemCat = document.getElementById('tiger');
+var elemCounter = document.getElementById('counter');
+
+
+// Create DOM HTML Element Nodes
+// Create a generic <p> node
+var para = document.createElement("p");
+// Create a text node
+var t = document.createTextNode(count);
+
+// Append Nodes and write Default Count
+// Append the text to <p>
+para.appendChild(t);
+// Append <p> to <div>
+elemCounter.appendChild(para);
+
+// Updates Click Count View
+elemCat.addEventListener('click', function(){
+	// the element has been clicked...
+	count++;
+	// Overwrites previous Count
+	para.innerHTML = count;
+
+
+}, false);
