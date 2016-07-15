26f907ff210ccab88b3b05383c5302d160c08fa9
George Fischer <geosynchronous@mac.com>
2016-07-15 11:44:18 -0600

    chore: main.js outline comments

--- CourseWork/catclicker-premium/js/main.js
+++ CourseWork/catclicker-premium/js/main.js
@@ -1,3 +1,32 @@
 // Cat Clicker Premium by George Fischer
 // Udacity Mini-Project
 
+// THE CODING PLAN
+// (DATA in data.js)
+    // - Generate
+    //     - DIVs
+    //         - Title
+    //         - Menu Container
+    //             - Directions
+    //             - Cat List
+    //                 -Individual Cat Names
+    //         - Data Containers (5 Cats)
+    //             - Description Container
+    //                 -Cat Name
+    //                 - Click Count (default Value = 0)
+    //             - Image
+    // - Append
+    //     - Title
+    //     - Menu Container
+    //     - Data Conainer (first Cat)
+
+    // - Invoke Event Handler
+    //     - Clicked Image Response
+    //         - Cat Selected
+    //         - Counter Incremented
+    //         - Event Response
+    //             - Update Selected Cat Data Content to screen (.innerHTML)
+    //                 - Cat Name
+    //                 - Click Count
+    //                 - Image
+
