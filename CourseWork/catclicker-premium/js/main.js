df5a6fb2983f1930e17adefa9544ab9d99396693
George Fischer <geosynchronous@mac.com>
2016-07-16 14:33:12 -0600

    docs: improved comments in js

--- CourseWork/catclicker-premium/js/main.js
+++ CourseWork/catclicker-premium/js/main.js
@@ -2,33 +2,34 @@
 // Udacity Mini-Project
 
 // LIST OF CATS
-// IIFE for creating and appending list in "Choose Cat" menu
+// IIFE for creating, appending and selecting list in "Choose Cat" menu
+var makeCatList = function() {
+
+    // Create and Append List
+    var catListItem = "",
+        catListElement,
+        num;
+
+    var ul = document.getElementById("catList");
+
+    for (var i = 0; i < data.cat.length; i++) {
+        num = i;
+        catListItem = "<li>" + data.cat[i].name + "</li>";
+        catListElement = document.createElement("li");
+        catListElement.innerHTML = catListItem;
+        ul.appendChild(catListElement);
+
+        // IIFE for each list element with eventListener  and with unique num value
+        // ... and when we click, alert the value of `num`
+        catListElement.addEventListener('click', (function(numCopy) {
+            return function() {
+            alert(numCopy);
+            };
+        })(num));
+    }
+}();
 
 
-    // Returns a string with cat names nested inside <li> numbered id tags
-    var makeCatList = function() {
-        var catListItem = "",
-            catListElement;
-
-        var ul = document.getElementById("catList");
-
-        for (var i = 0; i < data.cat.length; i++) {
-            catListItem = "<li>" + data.cat[i].name + "</li>";
-            catListElement = document.createElement("li");
-            catListElement.innerHTML = catListItem;
-            ul.appendChild(catListElement);
-        }
-        // return cats;
-    }();
-
-    // var li =
-
-    //     // ... and when we click, alert the value of `num`
-    // elem.addEventListener('click', (function(numCopy) {
-    //     return function() {
-    //         alert(numCopy);
-    //     };
-    // })(num));
 
 
 
