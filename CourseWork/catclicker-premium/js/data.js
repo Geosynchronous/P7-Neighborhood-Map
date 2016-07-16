60d0bf728b533473e4936633a1e0a207a74aff7f
George Fischer <geosynchronous@mac.com>
2016-07-15 20:16:57 -0600

    feat: Cat Data & image files
    also replaced some HTML classes with ids

--- CourseWork/catclicker-premium/js/data.js
+++ CourseWork/catclicker-premium/js/data.js
@@ -5,26 +5,58 @@ var dataConstructor = function() { 
 
 	var self = this; 
 
-	self.title = "This is the title"; 
-
 	self.cat = [
-	{  
-		name: "Cat1", 
-		description: "description", 
-		images: "URL", 
-		count: 0 
-	}, 
-	{ 
-		name: "Cat2", 
-		description: "description", 
-		images: "URL", 
-		count: 0 
-	} ];
+		{  
+			name: "Chicago", 
+			images: "images/chicago.jpg", 
+			count: 0 
+		}, 
+		{  
+			name: "Chill", 
+			images: "images/chill.jpg", 
+			count: 0 
+		}, 
+		{  
+			name: "Chops", 
+			images: "images/chops.jpg", 
+			count: 0 
+		}, 
+		{  
+			name: "Mamasita", 
+			images: "images/mamasita.jpg", 
+			count: 0 
+		}, 
+			{  
+			name: "Predator", 
+			images: "images/predator.jpg", 
+			count: 0 
+		}, 
+		{  
+			name: "Siberian", 
+			images: "images/siberian.jpg", 
+			count: 0 
+		}, 
+		{  
+			name: "Watch", 
+			images: "images/watch.jpg", 
+			count: 0 
+		}, 
+		{  
+			name: "Wet", 
+			images: "images/wet.jpg", 
+			count: 0 
+		}, 
+		{ 
+			name: "Woods", 
+			images: "images/woods.jpg", 
+			count: 0 
+		} ];
 };
 
 // Create Encapsulated Cat Data variables
 var data = new dataConstructor();  
 
-//Example chained encapsulated variable array element
+//TEMP
+//Example chained encapsulated variable array element that is created
 console.log(data.cat[0].name);
 
