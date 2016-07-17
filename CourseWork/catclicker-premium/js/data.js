07c9d17121a38177c6a32b456dcefd4da458dc3f
George Fischer <geosynchronous@mac.com>
2016-07-17 10:39:21 -0600

    docs: Update Images & Comments

--- CourseWork/catclicker-premium/js/data.js
+++ CourseWork/catclicker-premium/js/data.js
@@ -9,47 +9,38 @@ var dataConstructor = function() { 
 		{  
 			name: "Chicago", 
 			images: "images/chicago.jpg", 
-			count: 0 
 		}, 
 		{  
 			name: "Chill", 
 			images: "images/chill.jpg", 
-			count: 0 
 		}, 
 		{  
 			name: "Chops", 
 			images: "images/chops.jpg", 
-			count: 0 
 		}, 
 		{  
 			name: "Mamasita", 
 			images: "images/mamasita.jpg", 
-			count: 0 
 		}, 
 			{  
 			name: "Predator", 
 			images: "images/predator.jpg", 
-			count: 0 
 		}, 
 		{  
 			name: "Siberian", 
 			images: "images/siberian.jpg", 
-			count: 0 
 		}, 
 		{  
 			name: "Watch", 
 			images: "images/watch.jpg", 
-			count: 0 
 		}, 
 		{  
 			name: "Wet", 
 			images: "images/wet.jpg", 
-			count: 0 
 		}, 
 		{ 
 			name: "Woods", 
 			images: "images/woods.jpg", 
-			count: 0 
 		} ];
 };
 
