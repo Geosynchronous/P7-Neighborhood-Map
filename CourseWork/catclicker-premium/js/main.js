2dbccd3625c447824bdfd80287e27c81c9b0f898
George Fischer <geosynchronous@mac.com>
2016-07-16 18:30:39 -0600

    refactor: List selection & Image Element Updates

--- CourseWork/catclicker-premium/js/main.js
+++ CourseWork/catclicker-premium/js/main.js
@@ -1,29 +1,42 @@
 // Cat Clicker Premium by George Fischer
-// Udacity Mini-Project
+// Yet another very cool Udacity Mini-Project
 
-// LIST OF CATS
-// IIFE for creating, appending and selecting list in "Choose Cat" menu
-var makeCatList = function() {
+// IIFE: Setup, UI and Updates
+// Generates needed DOM Elements
+// Loads DOM Elements to Page
+// Event Listener Triggered Content Updates
+var CatClickerPremium = function() {
 
-    // Create and Append List
+    // Get DOM Elements
+    var ul = document.getElementById("catList");
+    var catNameElement = document.getElementById("catName");
+    var catCountElement = document.getElementById("catClickCount");
+    var catImageElement = document.getElementById("catImage");
+
     var catListItem = "",
         catListElement,
         num;
 
-    var ul = document.getElementById("catList");
-
+    // GENERATE & UPDATE ELEMENT CONTENT
+    // Menu & Image Container Element Content as needed
     for (var i = 0; i < data.cat.length; i++) {
         num = i;
         catListItem = "<li>" + data.cat[i].name + "</li>";
         catListElement = document.createElement("li");
         catListElement.innerHTML = catListItem;
+
+        // MENU LIST ITEM Appended
         ul.appendChild(catListElement);
 
-        // IIFE for each list element with eventListener  and with unique num value
+        // UPDATES IMAGE CONTAINER CONTENTS
+        // IIFE for each list element with eventListener and with unique num value
         // ... and when we click, alert the value of `num`
+        // Slick Trick
         catListElement.addEventListener('click', (function(numCopy) {
             return function() {
-            alert(numCopy);
+            catNameElement.innerHTML = data.cat[numCopy].name;
+            catImageElement.innerHTML = "<img src=" + data.cat[numCopy].images + ">";
+            catCountElement.innerHTML = data.cat[numCopy].count;
             };
         })(num));
     }
