a41b79a8495d6f5d0f121666e347de64a4c75b55
George Fischer <geosynchronous@mac.com>
2016-07-25 12:56:57 -0600

    feat: with binding & $parent
    Makes coding less cluttered

--- CourseWork/ud989-cat-clicker-ko-starter-master/js/app.js
+++ CourseWork/ud989-cat-clicker-ko-starter-master/js/app.js
@@ -33,7 +33,7 @@ var ViewModel = function() {
 	this.currentCat = ko.observable( new cat());
 
 	this.incrementCounter = function() {
-		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+		this.clickCount(this.clickCount() + 1);
 	}
 }
 
