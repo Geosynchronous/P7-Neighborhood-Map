34f420779cb7c3b6f0813893f4b1a79cc8b3e60b
George Fischer <geosynchronous@mac.com>
2016-07-27 17:03:50 -0600

    feat: priority button

--- CourseWork/ud989-todo-app-master/js/views/todo-view.js
+++ CourseWork/ud989-todo-app-master/js/views/todo-view.js
@@ -19,6 +19,7 @@ var app = app || {};
 		events: {
 			'click .toggle': 'toggleCompleted',
 			'dblclick label': 'edit',
+			'click .priority-btn': 'togglePriority',
 			'click .destroy': 'clear',
 			'click .edit-btn': 'edit',
 			'keypress .edit': 'updateOnEnter',
@@ -51,6 +52,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -64,6 +66,11 @@ var app = app || {};
 			return this.model.get('completed') ?
 				app.TodoFilter === 'active' :
 				app.TodoFilter === 'completed';
+		},
+
+		// Toggle the `"prioriry"` state of the model.
+		togglePriority: function () {
+			this.model.togglePriority();
 		},
 
 		// Toggle the `"completed"` state of the model.
