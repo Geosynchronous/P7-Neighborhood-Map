3c08d104d837c79b9edc2e74ff4c2edbdb1b059a
George Fischer <geosynchronous@mac.com>
2016-11-14 14:08:57 -0700

    refactor: globalData.js created
    Separates global vars out of app.js

--- Round_River_Map_Project/js/app.js
+++ Round_River_Map_Project/js/app.js
@@ -19,100 +19,6 @@
 
 
 
-// ***GLOBAL VARIABLES***
-var map,
-    largeInfowindow,
-    bounds,
-    highlightedIcon,
-    defaultIcon,
-    styles = [{
-        featureType: 'landscape',
-        elementType: 'natural.landcover',
-        stylers: [{
-            visibility: 'on'
-        }]
-    }, {
-        featureType: 'administrative',
-        elementType: 'country',
-        stylers: [{
-            visibility: 'off'
-        }]
-    }, {
-        featureType: 'water',
-        stylers: [{
-            color: '#19a0d8'
-        }]
-    }, {
-        featureType: 'administrative',
-        elementType: 'labels.text.stroke',
-        stylers: [{
-            color: '#ffffff'
-        }, {
-            weight: 6
-        }]
-    }, {
-        featureType: 'administrative',
-        elementType: 'labels.text.fill',
-        stylers: [{
-            color: '#e85113'
-        }, {
-            visibility: 'off'
-        }]
-    }, {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{
-            color: '#efe9e4'
-        }, {
-            lightness: -40
-        }]
-    }, {
-        featureType: 'transit.station',
-        stylers: [{
-            weight: 9
-        }, {
-            hue: '#e85113'
-        }]
-    }, {
-        featureType: 'road.highway',
-        elementType: 'labels.icon',
-        stylers: [{
-            visibility: 'off'
-        }]
-    }, {
-        featureType: 'water',
-        elementType: 'labels.text.stroke',
-        stylers: [{
-            lightness: 100
-        }, {
-            visibility: 'off'
-        }]
-    }, {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{
-            lightness: -100
-        }, {
-            visibility: 'off'
-        }]
-    }, {
-        featureType: 'poi',
-        elementType: 'geometry',
-        stylers: [{
-            visibility: 'on'
-        }, {
-            color: '#f0e4d3'
-        }]
-    }, {
-        featureType: 'road.highway',
-        elementType: 'geometry.fill',
-        stylers: [{
-            color: '#efe9e4'
-        }, {
-            lightness: -25
-        }]
-    }];
-
 
 
 // ***VIEW-MODEL***
@@ -538,19 +444,6 @@ function AppViewModel() {
 // ***GOOGLE MAPS***
 // Maps not compatible with KNOCKOUT
 // Hence code seperate from VIEW MODEL
-
-// Set Up Global Weather Data Functions
-var weatherData,
-    apiPath = 'http://api.openweathermap.org/data/2.5/weather?',
-    apiKey = '&APPID=db6d60dca9b88a47a0884b8ff753b7f6',
-    units = '&units=imperial',
-    gps,
-    currentTemp,
-    maxTemp,
-    minTemp,
-    humidity,
-    description,
-    weatherImage;
 
 // Loads Infowindow content after marker has been clicked
 function loadContent(marker) {
