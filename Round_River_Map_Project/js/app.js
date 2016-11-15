4898e1ba5c4cf0fb7087dc6ab38616e1115d3901
George Fischer <geosynchronous@mac.com>
2016-11-15 02:07:07 -0700

    docs: TODOS

--- Round_River_Map_Project/js/app.js
+++ Round_River_Map_Project/js/app.js
@@ -33,3 +33,33 @@ var vm = new AppViewModel();
 ko.applyBindings(vm);
 
 
+
+
+
+
+
+
+
+// TODO -
+// - Seperate App Data into seperate files
+//      - JSON
+//      - JS
+// - Set-Up GULP
+//      - minify
+//      - concat
+//      - lint
+//      - compress graphics
+//      - watch for change
+// - Load onto my Web Server
+//      - http://geosynchronous.us/roundrivermapapp...
+// - Set-Up Bower
+//      - https://bower.io/
+// - Set-Up Modular PKG Manager
+//      - http://browserify.org/
+//      - http://requirejs.org/
+//      - http://durandaljs.com/
+// - Present App to Round River
+
+// Knockout is mainly used together with PHP and ASP.NET back-ends. It is good to couple knockout with libraries such as RequireJS or Browserify for module loading and dependencies, and CrossroadsJS or RouterJS for routes in order to provide comparable functionality to other MVVM frameworks such as AngularJS or EmberJS. You can even get a full-fledged knockout based framework called DurandalJS. If you need to deal with multiple ViewModels in your SPA(Single-Page Application), I suggest Angular or Ember. Of course, you have to choose tools that help you to get the work done on time and in full.
+
+
